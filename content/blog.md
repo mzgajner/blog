058de0d111b1ed6a11a7fc64e5261068507e77fa
mzgajner <mato@zgajner.com>
2024-10-19 14:38:51 +0200

    Improve dark/light mode behaviour

--- content/blog.md
+++ content/blog.md
@@ -12,7 +12,7 @@ StackOverflow. That and my talks from random meetups.
 <ul>
   {%- for post in collections.blogposts -%}
     <li>
-      <span class="tag is-light is-family-monospace">{{ post.data.date | date: "%Y-%m-%d" }}</span>
+      <span class="tag is-family-monospace">{{ post.data.date | date: "%Y-%m-%d" }}</span>
       <a href="{{ post.url }}">{{ post.data.title }}</a></li>
   {%- endfor -%}
 </ul>
--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -7,8 +7,13 @@ export default async function(eleventyConfig) {
   // Pass static files through
   eleventyConfig.addPassthroughCopy({ "_static": "." });
 
+  // Generate two collections, one of blogposts and one of main nav items
   eleventyConfig.addCollection("blogposts", function (collectionApi) {
     return collectionApi.getFilteredByGlob("content/blog/*.md")
       .sort((a, b) => b.data.date - a.data.date);
+  });
+  eleventyConfig.addCollection("navbar_items", function (collectionApi) {
+    return collectionApi.getFilteredByTag('main-nav')
+      .sort((a, b) => a.data.position - b.data.position);
   });
 };
